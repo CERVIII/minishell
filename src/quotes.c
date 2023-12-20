5958b83be3c7ee8c3f1a3401faf52b2786298834
Federico Jose Diaz Guanoluiza <fdiaz-gu@c3r6s2.42madrid.com>
2023-12-20 10:58:48 +0100

    cleaned comments

--- src/quotes.c
+++ src/quotes.c
@@ -6,7 +6,7 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 10:36:04 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2023/12/18 16:13:30 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2023/12/18 16:16:29 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,52 +84,15 @@ void check_quotes(t_token **lst)
 {
 	int single_flag;
 	int double_flag;
-	// t_token *temp = *lst;
 
 	double_flag = 0;
 	single_flag = 0;
 	while ((*lst))
 	{
 		if ((*lst)->type == 6)
-		{
 			lst = double_quotes(lst, double_flag);
-			// printf("%sFIN: %s	FIN: %d\n", GREEN,(temp)->token, (temp)->type);
-			// printf("FIN: %s	FIN: %d%s\n", (temp)->next->token, (temp)->next->type, NORMAL);
-			// while (lst)
-			// {
-			// 	lst = lst->next;
-			// }
-			// double_flag = 1;
-			// if ((*lst)->next)
-			// {
-			// 	lst = &(*lst)->next;
-			// 	printf("A\n");
-			// 	while ((*lst)->next && (*lst)->type != 6)
-			// 		lst = &(*lst)->next;
-			// 	if ((*lst)->type == 6)
-			// 		double_flag = 0;
-			// }
-			// if (double_flag != 0)
-			// 	ft_err("ERROR: Unclosed quotes", 127);
-		}
 		else if ((*lst)->type == 7)
-		{
 			lst = single_quotes(lst, single_flag, double_flag);
-			// if (double_flag == 0)
-			// {
-			// 	single_flag = 1;
-			// 	if ((*lst)->next)
-			// 	{
-			// 		lst = &(*lst)->next;
-			// 		while ((*lst)->next && (*lst)->type != 7)
-			// 			lst = &(*lst)->next;
-			// 		if ((*lst)->type == 7)
-			// 			single_flag = 0;
-			// 	}
-			// }
-			// if (single_flag != 0)
-			// 	ft_err("ERROR: Unclosed quotes", 127);
-		}
 		lst = &(*lst)->next;		
 	}	
 }
