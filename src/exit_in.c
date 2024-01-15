1c62a657827dbe96c1c71660d4281eb6f51bcd22
Federico Jose Diaz Guanoluiza <fdiaz-gu@c2r3s4.42madrid.com>
2024-01-15 12:14:27 +0100

    finished exit

--- src/exit_in.c
+++ src/exit_in.c
@@ -3,39 +3,77 @@
 /*                                                        :::      ::::::::   */
 /*   exit_in.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fede <fede@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 14:34:18 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2024/01/14 19:48:30 by fede             ###   ########.fr       */
+/*   Updated: 2024/01/15 12:12:57 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+int ft_all_char(char **str)
+{
+    int i;
+
+    i = 0;
+    while(str[i])
+    {
+        if (!check_if_nb(str[i]))      
+            return (0);
+        i++;
+    }
+    return(1);
+}
+
+void ft_free_tools(t_tools *tools)
+{
+
+    int i = 0;
+    while(tools->env[i])
+    {
+        free(tools->env[i]);
+        i++;
+    }
+    free(tools->pwd);
+    free(tools->old_pwd);
+}
 void    ft_check_exit(char **str)
 {
     int code;
     
-    if (!str[1])
+    if (!str[0])
         code = 0;
-    else if (check_if_nb(str[1]))
-        code = ft_atoi(str[1]);
+    else if (!check_if_nb(str[0]))
+    {
+        code = ft_atoi(str[0]);
+        if (code < 0)
+        code = 255;
+    }     
     else
     {
         ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
-		ft_putstr_fd(str[1], STDERR_FILENO);
+		ft_putstr_fd(str[0], STDERR_FILENO);
 		ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
         code = 255;
     }
     exit(code);
 }
+
 int ft_exit(t_tools *tools, t_simple_cmds *simple_cmds)
 {
-	if (simple_cmds->str[2])
+    simple_cmds = malloc(sizeof(t_simple_cmds)); 
+	simple_cmds->str = malloc(3 * sizeof(char *));
+	simple_cmds->str[0] = ft_strdup("10");
+	simple_cmds->str[1] = ft_strdup("10214");
+	simple_cmds->str[2] = ft_strdup("101");
+    
+	if (simple_cmds->str[1] && simple_cmds->str[2] && !ft_all_char(simple_cmds->str))
     {
         ft_putendl_fd("exit: too many arguments\n", STDERR_FILENO);
         return (EXIT_FAILURE);
     }
+    ft_free_tools(tools);
     ft_check_exit(simple_cmds->str);
     return (EXIT_SUCCESS);
 }
