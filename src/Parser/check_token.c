d8e5fa40b0df0b18389f833d0dddccd7bd689049
Federico Jose Diaz Guanoluiza <fdiaz-gu@c1r15s5.42madrid.com>
2024-02-13 17:17:56 +0100

    fixed < infile cmd

--- src/Parser/check_token.c
+++ src/Parser/check_token.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   check_token.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pcervill <pcervill@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 11:50:22 by pcervill          #+#    #+#             */
-/*   Updated: 2024/02/12 10:16:22 by pcervill         ###   ########.fr       */
+/*   Updated: 2024/02/13 13:01:39 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,11 +46,11 @@ void	check_tokens(t_tools *tools, t_token **lst)
 		ft_err("Syntax error near unexpected token '|'", 127, tools);
 		minishell_loop(tools);
 	}
-	if ((*lst)->type > PIPE && (*lst)->type <= HERE_DOC)
-	{
-		ft_err("Syntax error near unexpected token 'newline'", 127, tools);
-		minishell_loop(tools);
-	}
+	// if ((*lst)->type > PIPE && (*lst)->type <= HERE_DOC)
+	// {
+	// 	ft_err("Syntax error near unexpected token 'newline'", 127, tools);
+	// 	minishell_loop(tools);
+	// }
 	while (*lst)
 	{
 		if (!check_redirects(lst, tools) || !check_broken_pipes(lst, tools))
