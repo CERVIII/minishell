8f82350f423ea82c1b64b6c97341c96eb6cc8968
Federico Jose Diaz Guanoluiza <fdiaz-gu@c1r5s5.42madrid.com>
2024-05-06 14:36:52 +0200

    fixed pwd

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 12:24:53 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2024/03/16 17:22:40 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2024/05/06 14:19:24 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,17 @@
 
 int	save_pwd(t_tools *tools)
 {
-	int	i;
+	int		i;
+	char	*buffer;
+	size_t	size;
 
 	i = 0;
-	tools->pwd = getcwd(NULL, sizeof(NULL));
-	// printf("");
+	size = 1024;
+	buffer = malloc(size);
+	if (!buffer)
+		return (1);
+	tools->pwd = getcwd(buffer, size);
+	printf("PWD: %s\n", tools->pwd);
 	while (tools->env[i])
 	{
 		if (ft_strncmp(tools->env[i], "OLDPWD=", 7) == 0)
